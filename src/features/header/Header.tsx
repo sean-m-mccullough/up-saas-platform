2fcb3fc79a2bb18b22c4a0afbad3c7392a58d57c
Sean McCullough <sean.m.mccullough@gmail.com>
2025-10-28 15:58:16 -0400

    add header feature

---
+++ src/features/header/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header() {
+    /* 
+        Simple header component.
+        Ideally, this would include navigation links and user info.
+    */
+    return (
+        <header>
+            <h1>UP!</h1>
+            <p>A SaaS Platform</p>
+        </header>
+    );
+}
+
+export default Header;
